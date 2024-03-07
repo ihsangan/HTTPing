b527d0b1807144eebf972de8157f42fde6789496
ihsangan <me@isan.eu.org>
2024-03-07 15:06:12 +0800

    darkmode

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const srv = document.getElementById("provider");
 function pingDNS() {
   const start = Date.now();
-  fetch(`${srv.value}?${start}`, {headers:{"user-agent":"ping"},cache: "no-cache"})
-     .then(response => {
-        const end = Date.now();
-        const timeTaken = end - start - 30;
-        //execution time can be different each devices
-        const pingElement = document.getElementById('ping');
-          pingElement.innerHTML = timeTaken;
-     })
-     .catch(error => {
-        console.log(error);
-      });
+  fetch(`${srv.value}?${start}`, {
+      cache: "no-cache"
+    })
+    .then(response => {
+      const end = Date.now();
+      const timeTaken = end - start - 30;
+      const pingElement = document.getElementById('ping');
+      pingElement.innerHTML = timeTaken;
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 window.onload = function() {
   pingDNS();
